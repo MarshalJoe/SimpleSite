f6e8c0cfe7dd3a90b45d264e96b5db55a16ad1db
MarshalJoe <joseph.charles.marshall@gmail.com>
2014-04-10 05:48:13 -0500

    Initial commit

---
+++ email.php
@@ -0,0 +1,14 @@
+<?php
+$toemail = 'joseph.charles.marshall@gmail.com';
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+mail($toemail, 'New message', $message, 'From: ' . $email);
+
+if(mail($toemail, 'New portfolio site message', $message, 'From: ' . $email)) {
+	echo "Message sent";
+} else {
+	echo 'Uh oh... Try joseph.charles.marshall@gmail.com';
+}
+?>
---
+++ mailtest.php
@@ -0,0 +1,12 @@
+<?php 
+
+if(mail("joseph.charles.marshall@gmail.com", 'test message', "test body") {
+	echo 'Your email was sent successfully.';
+} else {
+	echo 'There was a problem sending your email.';
+}
+
+
+
+
+?>
